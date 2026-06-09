0096ba3a0d64a7e6586ecb6e229a1a0685d7d29f
Aleksander Shkuratov <shk888@yandex.ru>
2026-06-09 14:52:36 +0300

    BCORE-8 Add LeadRepositoryTest.

--- src/main/java/ru/mentee/power/crm/model/Lead.java
+++ src/main/java/ru/mentee/power/crm/model/Lead.java
@@ -1,5 +1,7 @@
 package ru.mentee.power.crm.model;
 
+import java.util.Objects;
+
 public record Lead(
     String id,
     String email,
@@ -8,4 +10,20 @@ public record Lead(
     String status
 ) {
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ru.mentee.power.crm.domain.Lead lead = (ru.mentee.power.crm.domain.Lead) o;
+    return Objects.equals(id(), lead.id());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id());
+  }
 }
---
+++ src/test/java/ru/mentee/power/crm/repository/LeadRepositoryTest.java
@@ -0,0 +1,121 @@
+package ru.mentee.power.crm.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.mentee.power.crm.util.TestObjectFactory.DEFAULT_EMAIL;
+import static ru.mentee.power.crm.util.TestObjectFactory.DEFAULT_PHONE;
+
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.mentee.power.crm.model.Lead;
+
+@DisplayName("Тестирование LeadRepository")
+class LeadRepositoryTest {
+
+  private LeadRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    repository = new LeadRepository();
+  }
+
+  @Test
+  @DisplayName("findById: поиск существующего Lead по ID")
+  void shouldSaveAndFindLeadByIdWhenLeadSaved() {
+    // Given
+    String id = String.valueOf(UUID.randomUUID());
+    Lead lead = new Lead(id, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW");
+
+    // When
+    repository.save(lead);
+
+    // Then
+    assertThat(repository.findById(id)).isNotNull();
+  }
+
+  @Test
+  @DisplayName("findById: поиск существующего Lead по ID в пустом repository")
+  void shouldReturnNullWhenLeadNotFound() {
+    // Given
+    String id = String.valueOf(UUID.randomUUID());
+
+    // Then
+    assertThat(repository.findById(id)).isNull();
+  }
+
+  @Test
+  @DisplayName("findAll: проверка возвращения всех значений из repository")
+  void shouldReturnAllLeadsWhenMultipleLeadsSaved() {
+    // Given
+    String idFirst = String.valueOf(UUID.randomUUID());
+    String idSecond = String.valueOf(UUID.randomUUID());
+    String idThird = String.valueOf(UUID.randomUUID());
+
+    repository.save(
+        new Lead(idFirst, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW"));
+    repository.save(
+        new Lead(idSecond, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW"));
+    repository.save(
+        new Lead(idThird, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW"));
+
+    // Then
+    assertThat(repository.findAll()).hasSize(3);
+  }
+
+  @Test
+  @DisplayName("delete: проверка удаления существующего Lead в repository")
+  void shouldDeleteLeadWhenLeadExists() {
+    // Given
+    String id = String.valueOf(UUID.randomUUID());
+    Lead lead = new Lead(id, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW");
+    repository.save(lead);
+
+    // When
+    repository.delete(id);
+
+    // Then
+    assertThat(repository.findById(id)).isNull();
+    assertThat(repository.findAll()).isEmpty();
+  }
+
+  @Test
+  @DisplayName("Проверка замены данных сохранении у существующего Lead в repository")
+  void shouldOverwriteLeadWhenSavedWithSameId() {
+    // Given
+    String id = String.valueOf(UUID.randomUUID());
+    Lead lead = new Lead(id, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW");
+    repository.save(lead);
+
+    // When
+    Lead leadNewEmail = new Lead(id, "sale@gmail.com", DEFAULT_PHONE,
+        "TestCompany", "NEW");
+    repository.save(leadNewEmail);
+
+    // Then
+    assertThat(repository.findById(id))
+        .isNotNull()
+        .isEqualTo(leadNewEmail);
+    assertThat(repository.findAll()).hasSize(1);
+  }
+
+  @Test
+  @DisplayName("Проверка метода size()")
+  void shouldReturnSizeLeadsWhenMultipleLeadsSaved() {
+    // Given
+    String idFirst = String.valueOf(UUID.randomUUID());
+    String idSecond = String.valueOf(UUID.randomUUID());
+    String idThird = String.valueOf(UUID.randomUUID());
+
+    repository.save(
+        new Lead(idFirst, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW"));
+    repository.save(
+        new Lead(idSecond, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW"));
+    repository.save(
+        new Lead(idThird, DEFAULT_EMAIL, DEFAULT_PHONE, "TestCompany", "NEW"));
+    assertThat(repository.findAll()).hasSize(3);
+
+    // Then
+    assertThat(repository.size()).isEqualTo(3);
+  }
+}
